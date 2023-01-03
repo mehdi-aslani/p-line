03e89e88a056a7d6db9a807513f11f5953af6b8e
mehdi-aslani <aslani.mhd@gmail.com>
2023-01-03 07:39:39 +0330

    all

--- src/main/java/ir/peeco/pline/pline/InfoConfiguration.java
+++ src/main/java/ir/peeco/pline/pline/InfoConfiguration.java
@@ -1,10 +1,7 @@
 package ir.peeco.pline.pline;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import ir.peeco.pline.tools.ElementObject;
 import lombok.Data;
 
